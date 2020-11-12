b2a697a343b05669a17cda14fc7401cf41bb67ef
drewm1192 <44006682+drewm1192@users.noreply.github.com>
2020-11-12 17:31:03 -0500

    Adding initial files

---
+++ app.R
@@ -0,0 +1,44 @@
+#Appsilon Recruitment Task Dashboard
+library(shinyBS)
+library(shinyWidgets)
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(highcharter)
+library(dplyr)
+library(shinyjs)
+library(plotly)
+library(dplyr)
+library(shinycustomloader)
+library(formattable)
+library(tidyr)
+library(waiter)
+library(corazon)
+
+
+
+#####################
+#######THE UI########    
+#####################
+ui <- fluidPage(
+                 
+                 
+)
+
+
+
+####################
+#######SERVER#######
+####################
+
+
+server <- function(input, output,session) {  
+  
+  
+}
+
+
+
+shinyApp(ui, server)
+
+
---
+++ helperFunctions.r
@@ -0,0 +1,4 @@
+
+
+library(geosphere)
+distm (c(lon1, lat1), c(lon2, lat2), fun = distHaversine)
